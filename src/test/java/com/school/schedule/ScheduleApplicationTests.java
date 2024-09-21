72c5442db3cad6799dcad3a7b4cf21f49f22884e
PetarM09 <petarmilanovic0977@gmail.com>
2024-09-22 01:03:55 +0200

    first commit

---
+++ src/test/java/com/school/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
